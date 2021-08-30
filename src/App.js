a7f09032ed7d73148bd222ee70c4fe683180ca6c
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-08-30 23:22:02 +0900

    [Concept] Protection with PropTypes #7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, id }) {
+function Food({ name, id, rating }) {
   return (
-    <h1>
-      {id}. I like {name}
-    </h1>
+    <>
+      <h1>
+        {id}. I like {name}
+      </h1>
+      <h2>{rating} / 5</h2>
+    </>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 const foodILike = [
-  { name: "Kimchi", id: 0 },
-  { name: "dldld", id: 1 },
-  { name: "dkdk", id: 2 },
+  { name: "Kimchi", id: 0, rating: 5 },
+  { name: "dldld", id: 1, rating: 3 },
+  { name: "dkdk", id: 2, rating: 2 },
 ];
 
 function renderFood(dish) {
-  const { name, id } = dish;
-  return <Food name={name} id={id} key={id} />;
+  const { name, id, rating } = dish;
+  return <Food name={name} id={id} key={id} rating={rating} />;
 }
 function App() {
   return (
