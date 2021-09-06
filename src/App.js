c440441bc729b2098c0658c84fc927a1d884e81a
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-09-06 11:33:54 +0900

    [FEAT] Rendering a movie list #12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import PropTypes from "prop-types";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -8,16 +8,40 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    const movies = await axios.get(
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
     );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We're ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  genre={movie.genres}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 export default App;
