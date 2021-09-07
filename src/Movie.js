0c6527371b8dbec137c7d9b0735e1175cde465b2
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-09-07 23:33:26 +0900

    [FEAT] Styling & Add Genres #15

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, genre, summary, poster }) {
-  return <h4>{genre}</h4>;
+function Movie({ id, year, title, genres, summary, poster }) {
+  return (
+    <div className="movies_movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres_genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="move_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  genre: PropTypes.array.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
